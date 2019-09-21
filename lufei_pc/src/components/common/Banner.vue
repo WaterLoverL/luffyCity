ef1f147352421274a7e166066a04fb62e919b443
Chenyu Liu <chenyul07581@outlook.com>
2019-09-21 18:03:16 +0800

    前端获取轮播图广告的api数据并展示到页面中

--- lufei_pc/src/components/common/Banner.vue
+++ lufei_pc/src/components/common/Banner.vue
@@ -1,23 +1,33 @@
 <template>
   <el-carousel height="720px" :interval="3000" arrow="always">
-    <el-carousel-item>
-      <img src="/static/image/alex.jpeg" alt="">
-    </el-carousel-item>
-    <el-carousel-item>
-      <img src="/static/image/banner1.png" alt="">
-    </el-carousel-item>
-    <el-carousel-item>
-      <img src="/static/image/banner2.png" alt="">
-    </el-carousel-item>
-     <el-carousel-item>
-      <img src="/static/image/banner3.png" alt="">
+    <el-carousel-item v-for="(banner,index) in banner_list" :key="index">
+      <a :href="banner.link"><img :src="banner.image_url"></a>
+
     </el-carousel-item>
   </el-carousel>
 </template>
 
 <script>
-    export default {
+      export default {
         name: "Banner",
+        data(){
+            return {
+                banner_list:[]
+            }
+        },
+        created() {
+            this.get_banner_list();
+        },
+        methods:{
+            get_banner_list(){
+                // 获取轮播广告列表
+                this.$axios.get(`${this.$settings.HOST}/banner/`,{}).then(response=>{
+                  this.banner_list = response.data;
+                }).catch(error=>{
+                  console.log(error.response)
+                });
+            }
+        }
     }
 </script>
 
