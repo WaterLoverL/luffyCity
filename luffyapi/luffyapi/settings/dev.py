e95476538de486a96fda0da6bc347c655622d482
Chenyu Liu <chenyul07581@outlook.com>
2019-09-21 14:54:44 +0800

    配置mysql数据库

--- luffyapi/luffyapi/settings/dev.py
+++ luffyapi/luffyapi/settings/dev.py
@@ -73,12 +73,24 @@ WSGI_APPLICATION = 'luffyapi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "HOST": "127.0.0.1",
+        "PORT": 3306,
+        "USER": "luffy_user",
+        "PASSWORD": "luffy",
+        "NAME": "luffy",
     }
 }
+
 
 
 # Password validation
