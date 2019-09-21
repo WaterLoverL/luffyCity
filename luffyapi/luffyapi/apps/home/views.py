704537a8ed147f11f187de02e10af0ebc624b987
Chenyu Liu <chenyul07581@outlook.com>
2019-09-21 17:22:01 +0800

    add views.py/BannerListAPIView,serializers.py/BannerModelSerializer,urls.py/banner/

--- luffyapi/luffyapi/apps/home/views.py
+++ luffyapi/luffyapi/apps/home/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from .models import Banner
+from .serializers import BannerModelSerializer
 
-# Create your views here.
+
+class BannerListAPIView(ListAPIView):
+    queryset = Banner.objects.filter(is_show=True,is_deleted=False).order_by('-orders','-id')
+    serializer_class = BannerModelSerializer
+
